7930a0537d8395f065f177de420e8550ae12b9f8
yuhuijing <yuhuijing1@sina.com>
2023-08-27 14:40:52 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
+import java.io.*;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -43,7 +44,7 @@ public class DemoApplication {
 			return "upload"; // 返回上传表单的视图名称
 		}
 
-		@PostMapping("/yuhuijing.azurewebsites.net")
+		@PostMapping("/upload")
 		String handleFileUpload(MultipartFile file, Model model) {
 			try {
 				if (!file.isEmpty()) {
@@ -52,8 +53,6 @@ public class DemoApplication {
 
 					blobClient.upload(file.getInputStream(), file.getSize());
 					model.addAttribute("message", "简历上传成功111！");
-					// 上传文件
-					blobClient.upload(inputStream, file.getSize());
 
 					// 关闭输入流
 					inputStream.close();
@@ -67,7 +66,7 @@ public class DemoApplication {
 				model.addAttribute("message", "上传失败：" + e.getMessage());
 			}
 
-			return "upload"; // 返回上传结果视图
+			return "uploadResult"; // 返回上传结果视图
 		}
 	}
 }
