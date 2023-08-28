963975c1f9dfb70a13724bfe86643a0f8bea7082
yuhuijing <yuhuijing1@sina.com>
2023-08-28 15:59:45 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -30,7 +31,7 @@ public class DemoApplication {
 		// 初始化 BlobServiceClient
 		BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
 		// 容器名称
-		private final String containerName = "001upload";
+		private final String containerName = "0001upload";
 		// Blob 名称
 		private final String blobName = "upload001";
 		// 获取 BlobContainerClient
@@ -53,8 +54,16 @@ public class DemoApplication {
 
 					// 获取文件输入流
 					InputStream inputStream = file.getInputStream();
+					blobClient.upload(file.getInputStream(), file.getSize(), true);
 
-					blobClient.upload(file.getInputStream(), file.getSize());
+
+					// 设置正确的内容类型，根据文件的后缀名
+					String contentType = getFileContentType(file.getOriginalFilename());
+					blobClient.setHttpHeaders(new BlobHttpHeaders().setContentType(contentType));
+
+
+
+					//blobClient.upload(file.getInputStream(), file.getSize());
 					model.addAttribute("uploadSuccess", true);
 					model.addAttribute("message", "文件上传成功！");
 					model.addAttribute("fileName", uniqueFileName);
@@ -78,11 +87,12 @@ public class DemoApplication {
 		}
 
 		public class ErrorController {
-			@RequestMapping("/error")
+			@RequestMapping("/index")
 			public String handleError() {
-				return "error"; // 返回错误页面的视图名称
+				return "index"; // 返回错误页面的视图名称
 			}
 		}
+
 		// 格式化文件大小的方法
 		private String formatFileSize(long fileSize) {
 			if (fileSize <= 0) {
@@ -95,9 +105,18 @@ public class DemoApplication {
 			return String.format("%.1f %s", fileSize / Math.pow(1024, digitGroups), units[digitGroups]);
 		}
 
+		private String getFileContentType(String filename) {
+			String lowercaseFilename = filename.toLowerCase();
+			if (lowercaseFilename.endsWith(".pdf")) {
+				return "application/pdf";
+			} else if (lowercaseFilename.endsWith(".doc") || lowercaseFilename.endsWith(".docx")) {
+				return "application/msword";
+			} else {
+				// 默认内容类型，根据需要进行设置
+				return "application/octet-stream";
+			}
 
-
-
+		}
 	}
 }
 
